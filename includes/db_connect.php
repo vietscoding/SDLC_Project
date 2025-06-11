1613c9373e6d6708f6278f42a9e4c25662c93d91
DoanQuocViet <viet18725@gmail.com>
2025-06-11 17:18:25 +0700

    Sửa pass từ "1234" thành "".

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,7 +1,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "1234"; // nếu Workbench đặt mật khẩu thì ghi vào đây
+// $pass = "1234"; // nếu Workbench đặt mật khẩu thì ghi vào đây
+$pass = ""; //
 $dbname = "lms";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
